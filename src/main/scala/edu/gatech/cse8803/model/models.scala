f1983961877f7d1008ccb639a15441af26745b83
meerak <rhea@lawn-143-215-112-160.lawn.gatech.edu>
2015-03-17 15:08:18 -0400

    update observation model

--- src/main/scala/edu/gatech/cse8803/model/models.scala
+++ src/main/scala/edu/gatech/cse8803/model/models.scala
@@ -1,8 +1,8 @@
 package edu.gatech.cse8803.model
 
-case class Observation(personID:Int, observationConceptID: Int, observationDate: String, observationTime: String, valeAsNumber: Float, 
-    valueAsString:String, valueAsConceptId:Int, unitConceptId: Int, rangeLow: Float, rangeHigh: Float, observationTypeConceptId: Int, associatedProviderId: Int
-    visitOccurrenceId: Int, relevantConditionConceptId:Int, observationSourceValue:String, unitsSourceValue: String)
+case class Observation(person_id:Int, observation_concept_id: Int, observation_date: String, observation_time: String, vale_as_number: Float, 
+    value_as_string:String, value_as_concept_id:Int, unit_concept_id: Int, range_low: Float, range_high: Float, observation_type_concept_id: Int, associated_provider_id: Int
+    visit_occurrence_id: Int, relevant_condition_concept_id:Int, observation_source_value:String, units_source_value: String)
 
 case class Diagnostic(condition_occurrence_id: Integer, person_id: Integer, condition_concept_id: Integer, condition_start_date: String, condition_end_date: String, condition_type_concept_id: Integer, stop_reason: String, associated_provider_id: Integer, visit_occurrence_id: Integer, condition_source_value: String)
 
@@ -12,7 +12,7 @@ abstract class VertexProperty
 
 case class PatientProperty(person_id: Integer, gender_concept_id: Integer, year_of_birth: Integer, month_of_birth: Integer, day_of_birth: Integer, race_concept_id: Integer, ethnicity_concept_id: Integer, location_id: Integer, provider_id: Integer, care_site_id: Integer, person_source_value: String, gender_source_value: String, race_source_value: String, ethnicity_source_value: String) extends VertexProperty
 
-case class ObservationProperty(observationConceptID: Int) extends VertexProperty
+case class ObservationProperty(observation_concept_id: Int) extends VertexProperty
 
 case class DiagnosticProperty(icd9code: String) extends VertexProperty
 
@@ -22,9 +22,9 @@ abstract class EdgeProperty
 
 case class SampleEdgeProperty(name: String = "Sample") extends EdgeProperty
 
-case class PatientObservationProperty(observationDate: String, observationTime: String, valeAsNumber: Float, 
-    valueAsString:String, valueAsConceptId:Int, unitConceptId: Int, rangeLow: Float, rangeHigh: Float, observationTypeConceptId: Int, associatedProviderId: Int
-    visitOccurrenceId: Int, relevantConditionConceptId:Int, observationSourceValue:String, unitsSourceValue: String) extends EdgeProperty
+case class PatientObservationProperty(observation_date: String, observation_time: String, vale_as_number: Float, 
+    value_as_string:String, value_as_concept_id:Int, unit_concept_id: Int, range_low: Float, range_high: Float, observation_type_concept_id: Int, associated_provider_id: Int
+    visit_occurrence_id: Int, relevant_condition_concept_id:Int, observation_source_value:String, units_source_value: String) extends EdgeProperty
 
 case class PatientDiagnosticEdgeProperty(diagnostic: Diagnostic) extends EdgeProperty
 
