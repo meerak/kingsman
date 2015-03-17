a28a94744742bfe98507a13e099a3cb9b8651343
meerak <rhea@lawn-143-215-112-160.lawn.gatech.edu>
2015-03-17 14:55:53 -0400

    add observation

--- src/main/scala/edu/gatech/cse8803/model/models.scala
+++ src/main/scala/edu/gatech/cse8803/model/models.scala
@@ -4,7 +4,9 @@
 
 package edu.gatech.cse8803.model
 
-case class LabResult(patientID: String, date: Long, labName: String, loincCode: String, value: String)
+case class Observation(personID:Int, observationConceptID: Int, observationDate: String, observationTime: String, valeAsNumber: Float, 
+    valueAsString:String, valueAsConceptId:Int, unitConceptId: Int, rangeLow: Float, rangeHigh: Float, observationTypeConceptId: Int, associatedProviderId: Int
+    visitOccurrenceId: Int, relevantConditionConceptId:Int, observationSourceValue:String, unitsSourceValue: String)
 
 case class Diagnostic(patientID: String, date: Long, icd9code: String, sequence: Int)
 
@@ -14,7 +16,7 @@ abstract class VertexProperty
 
 case class PatientProperty(patientID: String, sex: String, dob: String, dod: String) extends VertexProperty
 
-case class LabResultProperty(testName: String) extends VertexProperty
+case class ObservationProperty(observationConceptID: Int) extends VertexProperty
 
 case class DiagnosticProperty(icd9code: String) extends VertexProperty
 
@@ -24,7 +26,9 @@ abstract class EdgeProperty
 
 case class SampleEdgeProperty(name: String = "Sample") extends EdgeProperty
 
-case class PatientLabEdgeProperty(labResult: LabResult) extends EdgeProperty
+case class PatientObservationProperty(observationDate: String, observationTime: String, valeAsNumber: Float, 
+    valueAsString:String, valueAsConceptId:Int, unitConceptId: Int, rangeLow: Float, rangeHigh: Float, observationTypeConceptId: Int, associatedProviderId: Int
+    visitOccurrenceId: Int, relevantConditionConceptId:Int, observationSourceValue:String, unitsSourceValue: String) extends EdgeProperty
 
 case class PatientDiagnosticEdgeProperty(diagnostic: Diagnostic) extends EdgeProperty
 
