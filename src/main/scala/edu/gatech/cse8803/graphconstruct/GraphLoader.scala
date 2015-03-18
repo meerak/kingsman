f1e27274d64936c2a6c8fe5c260e10acb4c39301
Sneha <siyengar8@gatech.edu>
2015-03-18 17:45:29 -0300

    Edges: lab + diag

--- src/main/scala/edu/gatech/cse8803/graphconstruct/GraphLoader.scala
+++ src/main/scala/edu/gatech/cse8803/graphconstruct/GraphLoader.scala
@@ -9,11 +9,8 @@ import edu.gatech.cse8803.enums._
 object GraphLoader {
   def load(patients: RDD[PatientProperty], medications: RDD[Medication], labResults:RDD[Observation], diagnostics: RDD[Diagnostic], rxnorm:RDD[Vocabulary], loinc: RDD[Vocabulary], snomed:RDD[Vocabulary], snomed_ancestors:RDD[ConceptAncestor], rxnorm_ancestors:RDD[ConceptAncestor]): Graph[VertexProperty, EdgeProperty] = {
 
-    //val sqlContext = new org.apache.spark.sql.SQLContext(patients.sparkContext)
-    //val sc = sqlContext.sparkContext
     val patientVertices: RDD[(VertexId, VertexProperty)] = patients.map(p => ((-p.person_id).toLong, p))
-    //val sqlContext = new org.apache.spark.sql.SQLContext(patients.sparkContext)
-    //val sc = sqlContext.sparkContext
+    
     val loincVertices: RDD[(VertexId, VertexProperty)] = loinc.map(a=>(a.concept_id.toLong, VocabularyProperty(a.concept_id)))
 
     val snomedVertices: RDD[(VertexId, VertexProperty)] = snomed.map(a=>(a.concept_id.toLong, VocabularyProperty(a.concept_id)))
@@ -22,46 +19,16 @@ object GraphLoader {
     val rxnormVertices: RDD[(VertexId, VertexProperty)] = rxnorm.map(a=>(a.concept_id.toLong, VocabularyProperty(a.concept_id)))
     val rxnormEdges: RDD[Edge[EdgeProperty]] = rxnorm_ancestors.map(a=>Edge(a.descendent_concept_id.toLong, a.ancestor_concept_id.toLong, ConceptAncestorEdgeProperty(Enumerations.ISA)))
 
-    /*val maxPatientID = patients.map(f=>f.patientID).toArray.maxBy(f=>f.toLong).toLong
-    
-    val labResultsProperty: RDD[VertexProperty] = labResults.map(a=>a.labName).distinct.map(a=>LabResultProperty(a))
-    val labCount = labResultsProperty.count.toLong
-    val labIndices  = sc.parallelize((maxPatientID +1) to (maxPatientID + labCount))
-    val labVertices = labIndices.zip(labResultsProperty)
-    
-    val prevCount = maxPatientID + labCount
-    val medicationsProperty: RDD[VertexProperty] = medications.map(m=>m.medicine).distinct.map(m=>MedicationProperty(m))
-    val medicationCount = medicationsProperty.count.toLong
-    val medicationIndices  = sc.parallelize((prevCount+1) to (prevCount + medicationCount))
-    val medicationVertices = medicationIndices.zip(medicationsProperty)
-    val prevCount1 = prevCount + medicationCount
-    val diagnosticsProperty: RDD[VertexProperty] = diagnostics.map(d=>d.icd9code).distinct.map(d=>DiagnosticProperty(d))
-    val diagnosticCount = diagnosticsProperty.count.toInt
-    val diagnosticIndices  = sc.parallelize((prevCount1+1) to (prevCount1 + diagnosticCount))
-    val diagnosticVertices = diagnosticIndices.zip(diagnosticsProperty)
-    
-    var vertices = patientVertices.union(labVertices).union(medicationVertices).union(diagnosticVertices)
-    
-    val maxLabResults = labResults.groupBy(x => (x.patientID, x.labName)).map(x => x._2.toArray.maxBy(y => y.date)).map(l => (l.labName, (l)))
-    val mapLabResults = labVertices.map(l => (l._2.asInstanceOf[LabResultProperty].testName, (l._1)))
-    val labEdges:RDD[Edge[EdgeProperty]] = maxLabResults.join(mapLabResults).map(e => Edge(e._2._1.patientID.toLong, e._2._2, PatientLabEdgeProperty(e._2._1)))
-    val labEdgesReverse:RDD[Edge[EdgeProperty]] = maxLabResults.join(mapLabResults).map(e => Edge(e._2._2, e._2._1.patientID.toLong, PatientLabEdgeProperty(e._2._1)))
-    val maxMedication = medications.groupBy(x => (x.patientID, x.medicine)).map(x => x._2.toArray.maxBy(y => y.date)).map(m => (m.medicine, (m)))
-    val mapMedication = medicationVertices.map(m => (m._2.asInstanceOf[MedicationProperty].medicine, (m._1)))
-    val medicationEdges:RDD[Edge[EdgeProperty]] = maxMedication.join(mapMedication).map(e => Edge(e._2._1.patientID.toLong, e._2._2, PatientMedicationEdgeProperty(e._2._1)))
-    val medicationEdgesReverse:RDD[Edge[EdgeProperty]] = maxMedication.join(mapMedication).map(e => Edge(e._2._2, e._2._1.patientID.toLong, PatientMedicationEdgeProperty(e._2._1)))
-    val maxDiagnostics = diagnostics.groupBy(x => (x.patientID, x.icd9code)).map(x => x._2.toArray.maxBy(y => y.date)).map(d=> (d.icd9code, (d)))
-    val mapDiagnostics = diagnosticVertices.map(v => (v._2.asInstanceOf[DiagnosticProperty].icd9code, (v._1)))
-    val diagnosticEdges:RDD[Edge[EdgeProperty]] = maxDiagnostics.join(mapDiagnostics).map(e => Edge(e._2._1.patientID.toLong, e._2._2, PatientDiagnosticEdgeProperty(e._2._1)))
-    val diagnosticEdgesReverse:RDD[Edge[EdgeProperty]] = maxDiagnostics.join(mapDiagnostics).map(e => Edge(e._2._2, e._2._1.patientID.toLong, PatientDiagnosticEdgeProperty(e._2._1)))
-    val edges = labEdges.union(medicationEdges).union(diagnosticEdges).union(labEdgesReverse).union(medicationEdgesReverse).union(diagnosticEdgesReverse)*/
+    val patlabEdges: RDD[Edge[EdgeProperty]] = labResults.map(x => Edge((-x.person_id).toLong, x.observation_concept_id.toLong,  PatientObservationProperty(x))) 
+
+    val patdiagEdges: RDD[Edge[EdgeProperty]] = diagnostics.map(x => Edge((-x.person_id).toLong, x.condition_concept_id.toLong,  PatientDiagnosticEdgeProperty(x)))
 
     val vertices = snomedVertices.union(rxnormVertices).union(loincVertices).union(patientVertices)
-    val edges = snomedEdges.union(rxnormEdges)
+    val edges = snomedEdges.union(rxnormEdges).union(patlabEdges).union(patdiagEdges)
     val graph: Graph[VertexProperty, EdgeProperty] = Graph(vertices, edges)
     println("all vertices: ", graph.vertices.count)
     println("all edges: ", graph.edges.count)
-    //edges.repartition(1).saveAsTextFile("EdgesBidirectional")
+    
     graph
   }
   /*
