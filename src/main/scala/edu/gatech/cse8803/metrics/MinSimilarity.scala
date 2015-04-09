dc34d7db9540ec108809ebb2d7f9c161dd87ba01
spisneha25 <spisneha25@users.noreply.github.com>
2015-04-09 15:55:27 -0300

    Added new metric

---
+++ src/main/scala/edu/gatech/cse8803/metrics/MinSimilarity.scala
@@ -0,0 +1,36 @@
+package edu.gatech.cse8803.metrics
+
+import edu.gatech.cse8803.model._
+import edu.gatech.cse8803.model.{EdgeProperty, VertexProperty}
+import org.apache.spark.graphx._
+import scala.collection.mutable.Map
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+
+object MinSimilarity 
+{
+  def MinSimilarityOneVsAll(graph: Graph[VertexProperty, EdgeProperty], patientID: String): List[String] = 
+  {
+        val bcSrcVertexId = graph.edges.sparkContext.broadcast(patientID.toLong)
+
+        val srcCharacteristic = graph.edges.filter{case Edge(srcId, dstId, edgeProperty) => srcId == bcSrcVertexId.value}.map{_.dstId}.collect().toSet
+        val bcSrcCharacteristic = graph.edges.sparkContext.broadcast(srcCharacteristic)
+
+        graph
+          .collectNeighbors(EdgeDirection.Out)
+          .filter{case (idx, neighbors) => neighbors.size > 0 && !neighbors.exists{case(_, p) => p.isInstanceOf[PatientProperty]}}
+          .mapValues
+          {
+            neighbors:Array[(VertexId, VertexProperty)] =>
+            val characteristic = neighbors.map(_._1).toSet
+            val intersect = bcSrcCharacteristic.value.intersect(characteristic)
+            if(characteristic.size == 0 && srcCharacteristic.size == 0) 0.0 
+            else intersect.size.toDouble / Math.min(characteristic.size.toDouble, srcCharacteristic.size.toDouble)
+          }
+          .filter(_._2 > 0.0)
+          .filter(x => x._1 != bcSrcVertexId.value)
+          .takeOrdered(100)(scala.Ordering.by(-_._2))
+          .map(x => (-1 * x._1).toString())
+          .toList
+  }
+}
