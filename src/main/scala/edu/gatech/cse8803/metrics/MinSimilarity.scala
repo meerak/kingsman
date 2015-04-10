c2b918c0ca54a337d139f0dd5034cd385eb7ccaf
spisneha25 <spisneha25@users.noreply.github.com>
2015-04-10 19:31:42 -0300

    Done finally :)

--- src/main/scala/edu/gatech/cse8803/metrics/MinSimilarity.scala
+++ src/main/scala/edu/gatech/cse8803/metrics/MinSimilarity.scala
@@ -9,28 +9,35 @@ import org.apache.spark.rdd.RDD
 
 object MinSimilarity 
 {
-  def MinSimilarityOneVsAll(graph: Graph[VertexProperty, EdgeProperty], patientID: String): List[String] = 
+  def MinSimilarityOneVsAll(graph: Graph[VertexProperty, EdgeProperty], patientID: String, casecontrol: List[String]): List[String] = 
   {
         val bcSrcVertexId = graph.edges.sparkContext.broadcast(patientID.toLong)
 
         val srcCharacteristic = graph.edges.filter{case Edge(srcId, dstId, edgeProperty) => srcId == bcSrcVertexId.value}.map{_.dstId}.collect().toSet
         val bcSrcCharacteristic = graph.edges.sparkContext.broadcast(srcCharacteristic)
 
-        graph
+        var neighborgraph =  graph
           .collectNeighbors(EdgeDirection.Out)
-          .filter{case (idx, neighbors) => neighbors.size > 0 && idx < 0}
-          .mapValues
-          {
-            neighbors:Array[(VertexId, VertexProperty)] =>
-            val characteristic = neighbors.map(_._1).toSet
-            val intersect = bcSrcCharacteristic.value.intersect(characteristic)
-            if(characteristic.size == 0 && srcCharacteristic.size == 0) 0.0 
-            else intersect.size.toDouble / Math.min(characteristic.size.toDouble, srcCharacteristic.size.toDouble)
-          }
-          .filter(_._2 > 0.0)
-          .filter(x => x._1 != bcSrcVertexId.value)
-          .takeOrdered(100)(scala.Ordering.by(-_._2))
-          .map(x => (-1 * x._1).toString())
-          .toList
+          .filter{case (idx, neighbors) => neighbors.size > 0 && idx < 0 && !neighbors.exists{case(_, p) => p.isInstanceOf[PatientProperty]}}
+
+        if(casecontrol != null)
+        {
+           neighborgraph.filter{case (idx, neighbors) => casecontrol.contains((-1 * idx).toString)}
+        }
+      
+        neighborgraph
+        .mapValues
+        {
+          neighbors:Array[(VertexId, VertexProperty)] =>
+          val characteristic = neighbors.map(_._1).toSet
+          val intersect = bcSrcCharacteristic.value.intersect(characteristic)
+          if(characteristic.size == 0 && srcCharacteristic.size == 0) 0.0 
+          else intersect.size.toDouble / Math.min(characteristic.size.toDouble, srcCharacteristic.size.toDouble)
+        }
+        .filter(_._2 > 0.0)
+        .filter(x => x._1 != bcSrcVertexId.value)
+        .takeOrdered(10)(scala.Ordering.by(-_._2))
+        .map(x => (-1 * x._1).toString())
+        .toList
   }
 }
